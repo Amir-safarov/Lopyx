459402d662e32c639e688724fedc65829f133067
212116 <212116@1305-016>
2025-02-21 16:09:42 +0300

    add little bit feature

--- Pages/AddEditProduct.xaml.cs
+++ Pages/AddEditProduct.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System.IO;
 using System;
 using System.Linq;
-using System.Text;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
 using WpfAppPaper.DataBase;
 using System.Collections.Generic;
+using System.Windows;
+using System.Text;
 
 namespace WpfAppPaper.Pages
 {
@@ -28,6 +29,12 @@ namespace WpfAppPaper.Pages
         {
             TypeProdCB.ItemsSource = App.DB.ProductType.ToList();
             TypeProdCB.DisplayMemberPath = "Name";
+
+            MaterialsCB.ItemsSource = App.DB.Materials.ToList();
+            MaterialsCB.DisplayMemberPath = "MaterialName";
+
+            if (_product == null)
+                AddMaterial.Visibility = Visibility.Hidden;
         }
 
         private void UpdateProdData()
@@ -36,13 +43,18 @@ namespace WpfAppPaper.Pages
             ArticlProdTB.Text = _product.Article;
             TypeProdCB.SelectedItem = _product.ProductType;
             NameProdTB.Text = $"{_product.ProductName}";
+            UpdateMaterials();
+            PriceProdTB.Text = _product.Price.ToString();
+        }
+
+        private void UpdateMaterials()
+        {
             List<Materials> materials = App.DB.Products
                 .Where(p => p.ProductID == _product.ProductID)
                     .Join(App.DB.Warehouse, p => p.ProductID, w => w.ProductID, (p, w) => w)
                     .Join(App.DB.Materials, w => w.MaterialID, m => m.MaterialID, (w, m) => m)
                     .ToList();
             MaterialProdList.ItemsSource = materials;
-            PriceProdTB.Text = _product.Price.ToString();
         }
 
         private BitmapImage GetimageSources(byte[] byteImage)
@@ -58,6 +70,52 @@ namespace WpfAppPaper.Pages
             }
             else
                 return new BitmapImage(new Uri(@"\Resources\picture.png", UriKind.Relative));
+        }
+
+        private void AddMaterial_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            AddNewMaterial();
+        }
+
+        private void AddNewMaterial()
+        {
+            Materials selectedMaterial = MaterialsCB.SelectedItem as Materials;
+            if (selectedMaterial == null)
+                return;
+            Warehouse warehouse = new Warehouse()
+            {
+                ProductID = _product.ProductID,
+                MaterialID = selectedMaterial.MaterialID,
+                Quantity = 1
+            };
+            App.DB.Warehouse.Add(warehouse);
+            UpdateMaterials();
+            //App.DB.SaveChanges();
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_product == null)
+            {
+                //TODO сделаит доавбление
+                StringBuilder stringBuilder = new StringBuilder();
+                if()
+            }
+            else
+            {
+                App.DB.SaveChanges();
+                MessageBox.Show("Запись обновленны");
+            }
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ProductListPage());
+        }
+
+        private void EditProdIMGBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
